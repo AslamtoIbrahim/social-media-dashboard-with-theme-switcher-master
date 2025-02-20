6ee06af276c4956276384a085631989f9e960375
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-02-20 16:52:58 +0100

    Added animation to switch button

--- src/app/Main/Switch.tsx
+++ src/app/Main/Switch.tsx
@@ -12,10 +12,12 @@ const Switch = ({ onClick, isOn }: prop) => {
       onClick={handleClick}
       className={`bg-lightTheme-toggle w-14 h-7 rounded-full flex items-center 
       cursor-pointer transition-all duration-300 ease-in-out
-    ${isOn ? `justify-start bg-toggle-gradient ` : "justify-end"} `}
+     ${isOn ? ` bg-toggle-gradient ` : ""} `}
     >
       <div
-        className={`w-5 h-5 mx-1 rounded-full bg-lightTheme-bg dark:bg-darkTheme-bg`}
+        className={`w-5 h-5 mx-1 rounded-full bg-lightTheme-bg dark:bg-darkTheme-bg 
+          transition-transform duration-300 transform ease-in-out 
+          ${isOn ? "translate-x-0" : "translate-x-7"}`}
       ></div>
     </div>
   );
